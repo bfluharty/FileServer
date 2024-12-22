e97da243a35bca64dac9de9696d12734910a02dc
Benjamin Fluharty <bfluharty3@gatech.edu>
2024-12-22 13:19:39 -0500

    Add logging

--- src/main/java/com/fluharty/fileserver/Controller.java
+++ src/main/java/com/fluharty/fileserver/Controller.java
@@ -3,6 +3,8 @@ package com.fluharty.fileserver;
 import com.fluharty.fileserver.exception.FileServerException;
 import com.fluharty.fileserver.model.UserFile;
 import com.fluharty.fileserver.service.UserFileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -20,6 +22,8 @@ import static com.fluharty.fileserver.utils.Constants.FILE_NOT_FOUND;
 
 @RestController
 public class Controller {
+    private static final Logger log = LoggerFactory.getLogger(Controller.class);
+
     private final UserFileService userFileService;
 
     public Controller(UserFileService userFileService) {
@@ -28,19 +32,23 @@ public class Controller {
 
     @GetMapping("/")
     public ResponseEntity<String> health() {
+        log.trace("Health check successful");
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/files")
     public ResponseEntity<List<String>> getFiles(@RequestHeader Map<String, String> headers) {
+        log.info("Retrieving files for user: {}", headers.get("user"));
         return ResponseEntity.ok(userFileService.getFiles());
     }
 
     @GetMapping("/download")
     public ResponseEntity<Resource> download(@RequestParam("filename") String filename, @RequestHeader Map<String, String> headers) {
+        log.info("Downloading file: {} for user: {}", filename, headers.get("user"));
         ByteArrayResource resource;
 
         if (!userFileService.getFiles().contains(filename)) {
+            log.error("File not found: {}", filename);
             throw new FileServerException(FILE_NOT_FOUND, null, HttpStatus.NOT_FOUND);
         } else {
             resource = new ByteArrayResource(userFileService.download(filename));
@@ -54,7 +62,9 @@ public class Controller {
 
     @PostMapping("/upload")
     public ResponseEntity<Void> upload(@RequestParam("file") MultipartFile file, @RequestHeader Map<String, String> headers) {
+        log.info("Uploading file: {} for user: {}", file.getOriginalFilename(), headers.get("user"));
         if (userFileService.getFiles().contains(file.getOriginalFilename())) {
+            log.error("File already exists: {}", file.getOriginalFilename());
             throw new FileServerException(FILE_ALREADY_EXISTS, null, HttpStatus.CONFLICT);
         }
 
@@ -64,7 +74,9 @@ public class Controller {
 
     @DeleteMapping("/delete")
     public ResponseEntity<Void> delete(@RequestParam("filename") String filename, @RequestHeader Map<String, String> headers) {
+        log.info("Deleting file: {} for user: {}", filename, headers.get("user"));
         if (!userFileService.getFiles().contains(filename)) {
+            log.error("File not found: {}", filename);
             throw new FileServerException(FILE_NOT_FOUND, null, HttpStatus.NOT_FOUND);
         } else {
             userFileService.delete(filename);
--- src/main/java/com/fluharty/fileserver/service/impl/UserFileServiceImpl.java
+++ src/main/java/com/fluharty/fileserver/service/impl/UserFileServiceImpl.java
@@ -7,6 +7,8 @@ import com.fluharty.fileserver.repository.UserFilesRepository;
 import com.fluharty.fileserver.service.AWSService;
 import com.fluharty.fileserver.service.UserFileService;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +21,8 @@ import static com.fluharty.fileserver.utils.Constants.*;
 
 @Service
 public class UserFileServiceImpl implements UserFileService {
+    private static final Logger log = LoggerFactory.getLogger(UserFileServiceImpl.class);
+
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
@@ -33,10 +37,13 @@ public class UserFileServiceImpl implements UserFileService {
     }
 
     public List<String> getFiles() {
+        log.debug("Retrieving files from cache");
         return listFilesCache.computeIfAbsent(bucketName, key -> {
             try {
+                log.debug("Retrieving files from AWS S3 bucket: {}", bucketName);
                 return awsService.listFiles(bucketName);
             } catch (AmazonClientException e) {
+                log.error("Failed to retrieve files from AWS S3 bucket: {}", e.getMessage());
                 throw new FileServerException(LIST_FILES, e.getMessage(), null);
             }
         });
@@ -44,28 +51,34 @@ public class UserFileServiceImpl implements UserFileService {
 
     @Transactional
     public void upload(UserFile userFile, MultipartFile file) {
-        userFilesRepository.save(userFile);
+//        userFilesRepository.save(userFile);
         try {
+            log.debug("Uploading file to AWS S3 bucket: {}", file.getOriginalFilename());
             awsService.uploadFile(bucketName, file.getOriginalFilename(), file.getSize(), file.getContentType(), file.getInputStream());
             refreshCacheAsync();
         } catch (AmazonClientException | IOException e) {
+            log.error("Failed to upload file to AWS S3 bucket: {}", e.getMessage());
             throw new FileServerException(FILE_UPLOAD, e.getMessage(), null);
         }
     }
 
     public byte[] download(String filename) {
         try {
+            log.debug("Downloading file from AWS S3 bucket: {}", filename);
             return awsService.downloadFile(bucketName, filename).toByteArray();
         } catch (AmazonClientException | IOException e) {
+            log.error("Failed to download file from AWS S3 bucket: {}", e.getMessage());
             throw new FileServerException(FILE_DOWNLOAD, e.getMessage(), null);
         }
     }
 
     public void delete(String filename) {
         try {
+            log.debug("Deleting file from AWS S3 bucket: {}", filename);
             awsService.deleteFile(bucketName, filename);
             refreshCacheAsync();
         } catch (AmazonClientException e) {
+            log.error("Failed to delete file from AWS S3 bucket: {}", e.getMessage());
             throw new FileServerException(FILE_DELETION, e.getMessage(), null);
         }
     }
@@ -73,10 +86,11 @@ public class UserFileServiceImpl implements UserFileService {
     private void refreshCacheAsync() {
         new Thread(() -> {
             try {
+                log.debug("Refreshing file list cache");
                 List<String> files = awsService.listFiles(bucketName);
                 listFilesCache.put(bucketName, files);
             } catch (AmazonClientException e) {
-                System.err.println("Failed to refresh file list cache: " + e.getMessage());
+                log.error("Failed to refresh file list cache: {}", e.getMessage());
             }
         }).start();
     }
